e69f1aed53d9ee5db7b4aa3d00edfcc6d5767706
Luiz G. <luiz.mwlo76@gmail.com>
2025-02-11 16:07:40 -0300

    Inserção da Classe Personagens na Solução

---
+++ AtividadeBranches/Models/Personagens.cs
@@ -0,0 +1,10 @@
+﻿namespace AtividadeBranches.Models
+{
+    public class Personagens
+    {
+        public int PersonagemId { get; set; }
+        public string Personagem { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
